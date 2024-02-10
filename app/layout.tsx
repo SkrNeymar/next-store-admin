249b71e6e7e70fd30742aa8cec26a341af447bbd
Grant_Cheng <cccguo97@gmail.com>
2024-02-10 18:53:30 +1000

    Add create stores modal and relevant api

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
 
 import { ModalProvider } from "@/providers/modalProviders"
+import { ToastProvider } from "@/providers/toastProviders"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToastProvider />
           <ModalProvider />
           {children}
         </body>
