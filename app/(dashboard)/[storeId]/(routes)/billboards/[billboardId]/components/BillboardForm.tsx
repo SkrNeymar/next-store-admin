3ff83a12f33bda6444362580f86727a0938c37a2
Grant_Cheng <cccguo97@gmail.com>
2024-02-12 18:04:57 +1000

    Add billboards create and edit

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
@@ -0,0 +1,176 @@
+"use client"
+import * as z from "zod"
+import { useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Trash } from "lucide-react"
+import { Billboard } from "@prisma/client"
+import toast from "react-hot-toast"
+import axios from "axios"
+import { useParams, useRouter } from "next/navigation"
+
+import { Heading } from "@/components/ui/heading"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import ImageUpload from "@/components/ui/image-upload"
+import { AlertModal } from "@/components/modals/alertModal"
+import { ApiAlert } from "@/components/ui/api-alert"
+import { useOrigin } from "@/hooks/useOrigin"
+
+interface BillboardFormProps {
+  initialData: Billboard | null
+}
+
+const formSchema = z.object({
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
+})
+
+type BillboardFormValues = z.infer<typeof formSchema>
+
+const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => {
+  const params = useParams()
+  const router = useRouter()
+  const origin = useOrigin()
+
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const title = initialData ? "Edit billboard" : "Create billboard"
+  const description = initialData ? "Edit a billboard" : "Add a billboard"
+  const toastSuccessMessage = initialData
+    ? "Billboard updated."
+    : "Billboard created."
+  const actionLabel = initialData ? "Save changes" : "Create"
+
+  const form = useForm<BillboardFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      label: "",
+      imageUrl: "",
+    },
+  })
+
+  const onSubmit = async (values: BillboardFormValues) => {
+    try {
+      setLoading(true)
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/billboards/${params.billboardId}`,
+          values
+        )
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, values)
+      }
+      router.refresh()
+      router.push(`/${params.storeId}/billboards`)
+      toast.success(toastSuccessMessage)
+    } catch (error) {
+      toast.error("Something went wrong.")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const onDelete = async () => {
+    try {
+      setLoading(true)
+      await axios.delete(
+        `/api/${params.storeId}/billboards/${params.billboardId}`
+      )
+      router.refresh()
+      router.push("/")
+      toast.success("Billboard deleted.")
+    } catch (error) {
+      toast.error("Make sure you removed all categories first.")
+    } finally {
+      setLoading(false)
+      setOpen(false)
+    }
+  }
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {/* Hide delete button when create new billboard */}
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="sm"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Label</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Billboard label"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {actionLabel}
+          </Button>
+        </form>
+      </Form>
+      <Separator />
+    </>
+  )
+}
+
+export default BillboardForm
