83d304c3d79b61ebfc9031024eec2e2d5d17e2e2
Grant_Cheng <cccguo97@gmail.com>
2024-02-14 18:09:21 +1000

    Update orders columns

--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -1,10 +1,7 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
-import { CellAction } from "./CellAction"
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 export type OrderColumn = {
   id: string
   phone: string
