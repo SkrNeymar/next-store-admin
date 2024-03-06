280092c9bace565d7497aa700ed6bb34c4a458cf
skrneymar <cccguo97@gmail.com>
2024-03-06 15:14:26 +1000

    Update cors header for get variant api

--- app/api/[storeId]/product/[variantId]/route.ts
+++ app/api/[storeId]/product/[variantId]/route.ts
@@ -2,10 +2,20 @@ import prismadb from "@/lib/prismadb"
 import { auth } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 
-export async function GET (
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+}
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders })
+}
+
+export async function GET(
   req: Request,
-  { params } : { params: {variantId: string } }
-){
+  { params }: { params: { variantId: string } }
+) {
   try {
     if (!params.variantId) {
       return new NextResponse("Variant id is required", { status: 400 })
@@ -16,12 +26,19 @@ export async function GET (
         id: params.variantId,
       },
       include: {
-        product: true,
+        size: true,
+        color: true,
+        product: {
+          include: {
+            images: true,
+          },
+        },
       },
-    });
+    })
 
-    return NextResponse.json(product)
-
+    return NextResponse.json(product, {
+      headers: corsHeaders,
+    })
   } catch (error) {
     console.log("[VARIANT GET]", error)
     return new NextResponse("Internal Server Error", { status: 500 })
