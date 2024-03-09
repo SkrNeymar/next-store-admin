26b4cf4100d685763625f84092f0612197a3aa07
skrneymar <cccguo97@gmail.com>
2024-03-09 15:49:25 +1000

    Add initial email to SignIn component

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,12 @@
+import React from "react"
 import { SignIn } from "@clerk/nextjs"
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <SignIn
+      initialValues={{
+        emailAddress: "admin@gmail.com",
+      }}
+    />
+  )
 }
